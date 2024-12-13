da3cc4eba700a52149ec68b04aee5a0d9d68e911
Erik Assum <erik.assum@ardoq.com>
2024-12-13 21:29:47 +0100

    Simplified a bit

--- src/usermanager/controllers/user.clj
+++ src/usermanager/controllers/user.clj
@@ -2,49 +2,27 @@
 
 (ns usermanager.controllers.user
   "The main controller for the user management portion of this app."
-  (:require [ring.util.response :as resp]
-            [selmer.parser :as tmpl]
-            [usermanager.model.user-manager :as model]
+  (:require [usermanager.model.user-manager :as model]
             [usermanager.db :as db]))
 
-(def ^:private changes
+(def changes
   "Count the number of changes (since the last reload)."
   (atom 0))
 
-(defn ->db [req]
-  (-> req :application/db))
-
-(defn render-page
-  "Each handler function here adds :application/view to the request
-  data to indicate which view file they want displayed. This allows
-  us to put the rendering logic in one place instead of repeating it
-  for every handler."
-  [req]
-  (let [data (assoc (:params req) :changes @changes)
-        view (:application/view req "default")
-        html (tmpl/render-file (str "views/user/" view ".html") data)]
-    (-> (resp/response (tmpl/render-file "layouts/default.html"
-                                         (assoc data :body [:safe html])))
-        (resp/content-type "text/html"))))
-
 (defn reset-changes
-  [req]
+  []
   (reset! changes 0)
-  (assoc-in req [:params :message] "The change tracker has been reset."))
+  {:message "The change tracker has been reset."})
 
-(defn default
-  [req]
-  (assoc-in req [:params :message]
-                (str "Welcome to the User Manager application demo! "
-                     "This uses just Compojure, Ring, and Selmer.")))
+(def default
+  {:message (str "Welcome to the User Manager application demo! "
+                 "This uses just Compojure, Ring, and Selmer.")})
 
 (defn delete-by-id
   "Compojure has already coerced the :id parameter to an int."
-  [req]
+  [db id]
   (swap! changes inc)
-  (db/delete! (->db req) (model/delete-by-id
-                          (get-in req [:params :id])))
-  (resp/redirect "/user/list"))
+  (db/delete! db (model/delete-by-id id)))
 
 (defn edit
   "Display the add/edit form.
@@ -52,33 +30,26 @@
   If the :id parameter is present, Compojure will have coerced it to an
   int and we can use it to populate the edit form by loading that user's
   data from the addressbook."
-  [req]
-  (let [db   (->db req)
-        user (when-let [id (get-in req [:params :id])]
+  [db id]
+  (let [user (when id
                (db/query-one! db (model/user-by-id id)))
         departments (db/query! db model/all-departments)]
-    (-> req
-        (update :params assoc
-                :user user
-                :departments departments)
-        (assoc :application/view "form"))))
+    {:user user
+     :departments departments}))
 
 (defn get-users
   "Render the list view with all the users in the addressbook."
-  [req]
-  (let [users (db/query! (->db req) model/all-users)]
-    (-> req
-        (assoc-in [:params :users] users)
-        (assoc :application/view "list"))))
+  [db]
+  (let [users (db/query! db model/all-users)]
+    {:users users}))
 
 (defn save
   "This works for saving new users as well as updating existing users, by
   delegatin to the model, and either passing nil for :addressbook/id or
   the numeric value that was passed to the edit form."
-  [req]
+  [db user]
   (swap! changes inc)
-  (-> req
-      :params
+  (-> user
       ;; get just the form fields we care about:
       (select-keys [:id :first_name :last_name :email :department_id])
       ;; convert form fields to numeric:
@@ -88,5 +59,4 @@
       (->> (reduce-kv (fn [m k v] (assoc! m (db/->kw :addressbook k) v))
                       (transient {}))
            (persistent!)
-           (db/save! (->db req) :addressbook)))
-  (resp/redirect "/user/list"))
+           (db/save! db :addressbook))))
--- src/usermanager/main.clj
+++ src/usermanager/main.clj
@@ -28,60 +28,64 @@
   complexity that is Java's JDBC class hierarchy.
 
   This example uses a local SQLite database to store data."
-  (:require [compojure.coercions :refer [as-int]]
-            [compojure.core :refer [GET POST let-routes]]
-            [compojure.route :as route]
+  (:require [compojure.route :as route]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.defaults :as ring-defaults]
             [ring.util.response :as resp]
             [usermanager.controllers.user :as user-ctl]
             [usermanager.migration :as migration]
-            [next.jdbc :as jdbc])
+            [next.jdbc :as jdbc]
+            [selmer.parser :as tmpl]
+            [clojure.string :as str])
   (:gen-class))
 
-(defn my-middleware
-  "This middleware runs for every request and can execute before/after logic.
+(defn render-page
+  [data view]
+  (let [data (assoc data :changes @user-ctl/changes)
+        html (tmpl/render-file (str "views/user/" view ".html") data)]
+    (-> (resp/response (tmpl/render-file "layouts/default.html"
+                                         (assoc data :body [:safe html])))
+        (resp/content-type "text/html"))))
 
-  If the handler returns an HTTP response (like a redirect), we're done.
-  Else we use the result of the handler to render an HTML page."
-  [handler]
-  (fn [req]
-    (let [resp (handler req)]
-      (if (resp/response? resp)
-        resp
-        (user-ctl/render-page resp)))))
+(defn ->db [req]
+  (-> req :application/db))
 
-;; Helper for building the middleware:
-(defn- add-app-component
-  "Middleware to add your application component into the request. Use
-  the same qualified keyword in your controller to retrieve it."
-  [handler db]
-  (fn [req]
-    (handler (assoc req :application/db db))))
 
-;; This is Ring-specific, the specific stack of middleware you need for your
-;; application. This example uses a fairly standard stack of Ring middleware
-;; with some tweaks for convenience
-(defn middleware-stack
-  "Given the application component and middleware, return a standard stack of
-  Ring middleware for a web application."
-  [db app-middleware]
-  (fn [handler]
-    (-> handler
-        (app-middleware)
-        (add-app-component db)
-        (ring-defaults/wrap-defaults (-> ring-defaults/site-defaults
-                                         ;; disable XSRF for now
-                                         (assoc-in [:security :anti-forgery] false)
-                                         ;; support load balancers
-                                         (assoc-in [:proxy] true))))))
+(defn- ->id [uri]
+  (-> uri (str/split #"/") last Long/parseLong))
 
-;; This is the main web handler, that builds routing middleware
-;; from the application component (defined above). The handler is passed
-;; into the web server component (below).
-;; Note that Vars are used -- the #' notation -- instead of bare symbols
-;; to make REPL-driven development easier. See the following for details:
-;; https://clojure.org/guides/repl/enhancing_your_repl_workflow#writing-repl-friendly-programs
+(defn handle-default []
+  (-> user-ctl/default
+      (render-page "default")))
+
+(defn handle-reset-changes []
+  (-> (user-ctl/reset-changes)
+      (render-page "default")))
+
+(defn handle-delete-user [db id]
+  (user-ctl/delete-by-id db id)
+  (resp/redirect "/user/list"))
+
+(defn handle-edit [db]
+  (-> db
+      (user-ctl/edit nil)
+      (render-page "form")))
+
+(defn handle-edit-user [db id]
+  (-> db
+      (user-ctl/edit id)
+      (render-page "form")))
+
+(defn handle-get-users [db]
+  (-> db
+      user-ctl/get-users
+      (render-page "list")))
+
+(defn handle-save [db user]
+  (-> db
+      (user-ctl/save user))
+  (resp/redirect "/user/list"))
+
 (defn my-handler
   "Given the application component, return middleware for routing.
 
@@ -93,41 +97,36 @@
   and we need to pass in the application component at start up, we
   need to define our route handlers so that they can be parameterized."
   [db]
-  (let-routes [wrap (middleware-stack db #'my-middleware)]
-    (GET  "/"                        []              (wrap #'user-ctl/default))
-    ;; horrible: application should POST to this URL!
-    (GET  "/user/delete/:id{[0-9]+}" [id :<< as-int] (wrap #'user-ctl/delete-by-id))
-    ;; add a new user:
-    (GET  "/user/form"               []              (wrap #'user-ctl/edit))
-    ;; edit an existing user:
-    (GET  "/user/form/:id{[0-9]+}"   [id :<< as-int] (wrap #'user-ctl/edit))
-    (GET  "/user/list"               []              (wrap #'user-ctl/get-users))
-    (POST "/user/save"               []              (wrap #'user-ctl/save))
-    ;; this just resets the change tracker but really should be a POST :)
-    (GET  "/reset"                   []              (wrap #'user-ctl/reset-changes))
-    (route/resources "/")
-    (route/not-found "Not Found")))
+  (fn [{:keys [uri] :as req}]
+    (cond
+      (= uri "/") (#'handle-default)
+      (str/starts-with? uri "/user/delete") (#'handle-delete-user db (->id uri))
+      (= uri "/user/form") (#'handle-edit db)
+      (str/starts-with? uri "/user/form/") (#'handle-edit-user db (->id uri))
+      (= uri "/user/list") (#'handle-get-users db)
+      (= uri "/user/save") (#'handle-save db (:params req))
+      (= uri "/reset") (#'handle-reset-changes)
+      :else ((route/not-found "Not Found") req))))
 
-;; Standard web server component -- knows how to stop and start the
-;; web server (with the application component as a dependency, and
-;; the handler function as a parameter):
+(defn middleware-stack
+  "Given the application component and middleware, return a standard stack of
+  Ring middleware for a web application."
+  [db]
+  (-> (my-handler db)
+      (ring-defaults/wrap-defaults (-> ring-defaults/site-defaults
+                                         ;; disable XSRF for now
+                                       (assoc-in [:security :anti-forgery] false)
+                                         ;; support load balancers
+                                       (assoc-in [:proxy] true)))))
 
 (defn web-server
   "Return a WebServer component that depends on the application.
 
   The handler-fn is a function that accepts the application (Component) and
   returns a fully configured Ring handler (with middeware)."
-  [handler-fn db port]
-  (run-jetty (handler-fn db)
+  [handler-fn port]
+  (run-jetty handler-fn
              {:port port :join? false}))
-
-;; This is the piece that combines the generic web server component above with
-;; your application-specific component defined at the top of the file, and
-;; any dependencies your application has (in this case, the database):
-;; Note that a Var is used -- the #' notation -- instead of a bare symbol
-;; to make REPL-driven development easier. See the following for details:
-;; https://clojure.org/guides/repl/enhancing_your_repl_workflow#writing-repl-friendly-programs
-
 
 (def ^:private my-db
   "SQLite database connection spec."
@@ -140,15 +139,13 @@
 
   (def system (new-system 8888))
 
-  (alter-var-root #'system component/start)
-
-  (alter-var-root #'system component/stop)
+  (stop system)
 
   See the Rich Comment Form below."
   [port]
   (let [db (setup-database my-db)]
     {:db db
-     :web-server (web-server #'my-handler db port)}))
+     :web-server (web-server (middleware-stack db) port)}))
 
 (defn stop [system]
   (.stop (:web-server system)))
